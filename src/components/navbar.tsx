865719bd373d567c2276427f72990583fbd979b2
Nathan Cox <nrc4@hotmail.com>
2025-04-08 12:31:14 -0700

    Add logic to the navbar to highlight the chosen link.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { NavItem } from '@/data/navItems';
 // import { useLoggedIn } from '@/context/loggedIn';
@@ -8,16 +12,21 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ items }) => {
   // const isLoggedIn = useLoggedIn();
-
-  console.log({items});
+  const [pathname, setPathname] = useState<string>(usePathname());
+  const isActive = (pagename: string) => pathname === pagename ? 'text-blue-500' : '';
 
   return (
-    <nav className='bg-amber-200 flex flex-col items-center justify-end min-h-40 pb-6'>
-      <h1 className='text-black text-3xl font-bold'>Phone & Pin</h1>
-      <div className="flex items-center justify-content gap-4 border-t-2 border-black">
+    <nav className='flex flex-col items-center justify-end min-h-40 pb-6'>
+      <h1 className='text-white text-3xl font-bold'>Phone & Pin</h1>
+      <div className="flex items-center gap-4 border-t-2 border-white">
         {items.map(item => (
           item.type === 'public' && (
-            <Link key={item.id} href={item.url} className="text-black text-lg font-semibold">
+            <Link 
+              key={item.id} 
+              href={item.url} 
+              className= {`${isActive(item.url)} text-lg font-semibold`}
+              onClick={() => setPathname(item.url)}
+            >
               {item.name}
             </Link>
           )
