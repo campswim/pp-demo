4c6f15eec6750d6bd7971f532c8e0fab566130e1
Nathan Cox <nrc4@hotmail.com>
2025-04-08 11:06:52 -0700

    Clear the homepage of boilerplate.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <main className="bg-amber-200">
-      <div className='w-80 m-auto pt-16 bg-amber-600'>
-        <h1 className="text-3xl font-bold">Ian wuz ere</h1>
-      </div>
-    </main>
+    <div>
+      Home Page
+    </div>
   );
 }
