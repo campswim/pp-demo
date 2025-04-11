634e38b6e6871c3dc3ad536beb73425875e43f25
Nathan Cox <nrc4@hotmail.com>
2025-04-11 13:07:18 -0700

    Add a check of the auth cookie to layout and pass it to the log-in context.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,37 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/navbar";
-import { navItems } from '@/data/navItems';
-import { LoggedInProvider } from "@/context/loggedIn";
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+import Navbar from "@/components/navbar"
+import { navItems } from '@/data/navItems'
+import { LoggedInProvider } from "@/context/loggedIn"
+import { cookies } from 'next/headers'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "P&P Demo",
   description: "A unique system that allows users to log into accounts containing sensitive information by saying or entering a predetermined PIN via telephone.",
   keywords: 'phone, pin, security, authentication',
   authors: [{ name: "Nate Cox" }],
-};
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode}>) {
+  const cookieStore = await cookies()
+  const authCookie = cookieStore.get('auth')
+  const { role = 'guest' } = authCookie ? JSON.parse(authCookie.value) : {}
+
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <LoggedInProvider>
+        <LoggedInProvider initialRole={role}>
           <Navbar items={navItems} />
         </LoggedInProvider>
         <main className='flex flex-col w-[90%] m-auto min-h-screen'>
@@ -38,5 +39,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  );
+  )
 }
