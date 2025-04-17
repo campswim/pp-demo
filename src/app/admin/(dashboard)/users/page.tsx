c668557a5c43205f37fa703ef2772481cee3c9ff
Nathan Cox <nrc4@hotmail.com>
2025-04-17 16:04:12 -0700

    Refactor the users page to use validateAuthCookie and getUsers helpers.

--- src/app/admin/(dashboard)/users/page.tsx
+++ src/app/admin/(dashboard)/users/page.tsx
@@ -1,9 +1,11 @@
-import { getUsers } from '@/utils/userActions'
 import NewUserForm from '@/components/createUserForm'
 import UsersList from '@/components/usersList'
+import { validateAuthCookie  } from '@/utils/auth'
+import { getUsers } from '@/utils/userActions'
 
 export default async function Users() {
-  const users = await getUsers()
+  const decoded = await validateAuthCookie()
+    const users = await getUsers(decoded)
 
   return (
     <div className='flex flex-col items-center justify-between xl:flex-row lg:items-start lg:justify-center gap-10'>
