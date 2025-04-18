8ac7be6dba4813a5f4c3b9eba168d735992a8703
Nathan Cox <nrc4@hotmail.com>
2025-04-18 14:31:07 -0700

    Add commentary at the top os the users page.tsx; add a redirect to login or unauthorized based on the status of the user.

--- src/app/admin/(dashboard)/users/page.tsx
+++ src/app/admin/(dashboard)/users/page.tsx
@@ -1,11 +1,30 @@
+/**
+ * @file Users admin page.
+ * @description Restricted to authenticated users with the 'admin' role.
+ * Redirects unauthenticated users to /login.
+ * Redirects unauthorized users to /unauthorized.
+ * Lists all users.
+ * Add-user feature.
+ * @module /src/app/admin/users/page.tsx
+ */
+
 import NewUserForm from '@/components/createUserForm'
 import UsersList from '@/components/usersList'
+import { redirect } from 'next/navigation'
 import { validateAuthCookie  } from '@/utils/auth'
 import { getUsers } from '@/utils/userActions'
 
 export default async function Users() {
   const decoded = await validateAuthCookie()
-  const users = decoded ? await getUsers(decoded.role) : null
+
+  // Redirect to the log-in page, if there is no auth cookie.
+  if (!decoded) redirect('/login')
+
+  // Redirect to the unauthorized page, when a user's role is not admin.
+  const role = decoded?.payload?.role ?? 'guest'
+  if (role !== 'admin') redirect(`/unauthorized?role=${role}`)
+
+  const users = decoded ? await getUsers(role) : null
 
   return (
     <div className='flex flex-col items-center justify-between xl:flex-row lg:items-start lg:justify-center gap-10'>
