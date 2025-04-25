85a0201c06599b027c45632be6e2582db4326a5a
Nathan Cox <nrc4@hotmail.com>
2025-04-25 12:04:32 -0700

    Look for the expired-user-id cookie in logged-in context and call logout, when it's present.

--- src/context/loggedIn.tsx
+++ src/context/loggedIn.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import { createContext, useContext, useState, useEffect } from 'react'
-import { type JWTPayload } from 'jose'
+import { usePathname } from 'next/navigation'
+import { MyJWTPayload } from 'jose'
+import { validateAuthCookie } from '@/utils/auth'
+import { logout } from '@/utils/userActions'
 
 export type LoggedInUser = {
   userId: string
@@ -9,28 +12,61 @@ export type LoggedInUser = {
 }
 
 type LoggedInContextType = {
-  userId: string
+  userId: string | null
   setUserId: (userId: string) => void
-  role: string
+  role: string | null
   setRole: (role: string) => void
 }
 
 type LoggedInProviderProps = {
   children: React.ReactNode
-  user: (JWTPayload & Partial<{ userId: string; role: string }>) | null
+  user: (MyJWTPayload & Partial<{ userId: string; role: string }>) | null
 }
 
 const LoggedInContext = createContext<LoggedInContextType | undefined>(undefined)
 
 export function LoggedInProvider({ children, user }: LoggedInProviderProps) {
-  const [userId, setUserId] = useState<string>(user?.userId ?? '')
-  const [role, setRole] = useState<string>(user?.role ?? 'guest')
-  const initialRole = user?.role ?? 'guest'
+
+  console.log('-------------- LoggedInContext Start ------------------')
+
+  const [userId, setUserId] = useState<string | null>(user?.userId || null)
+  const [role, setRole] = useState<string | null>(user?.role || 'guest')
+  // const initialRole = role ?? 'guest'
+  const pathname = usePathname()
+
+  // useEffect(() => {
+  //   setUserId(user?.userId !== userId ? user?.userId ?? null : userId)
+  //   setRole(role !== initialRole ? initialRole : role)
+  // }, [user, initialRole, role, userId])
 
   useEffect(() => {
-    setUserId(user?.userId !== userId ? user?.userId ?? '' : userId)
-    setRole(role !== initialRole ? initialRole : role)
-  }, [user, initialRole, role, userId])
+    // Get the auth cookie.
+    const getAuthCookie = async () => {
+      const user: MyJWTPayload | null = await validateAuthCookie() 
+      
+      if (user) {
+        const newId: string | null = user?.userId ?? null
+        const newRole: string = user?.role ?? 'guest'
+        if (newId !== userId) {
+          setUserId(newId)
+          setRole(newRole)
+        }
+      } else {
+        const expiredId = document.cookie.split('; ').find(row => row.startsWith('user-id'))?.split('=')[1]
+        console.log({ expiredId })
+
+        if (expiredId) (async () => await logout(expiredId))()
+
+        setUserId(null)
+        setRole('guest')
+      }
+    }
+    getAuthCookie()
+  }, [userId, pathname])
+
+  console.log({userId, role})
+
+  console.log('-------------- LoggedInContext End ------------------')
 
   return (
     <LoggedInContext.Provider value={{ userId, setUserId, role, setRole }}>
