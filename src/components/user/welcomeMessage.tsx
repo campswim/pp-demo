193c6a2423d5f9b666acdc77d25831ae765cceb8
Nathan Cox <nrc4@hotmail.com>
2025-05-05 13:51:24 -0700

    Add username to the welcome message and refactor to render a message that's relevant to new registrations or repeated log-ins.

--- src/components/user/welcomeMessage.tsx
+++ src/components/user/welcomeMessage.tsx
@@ -4,12 +4,15 @@ import { useLoggedIn } from '@/context/loggedIn'
 import { useSearchParams } from 'next/navigation'
 
 const WelcomeMessage = () => {
-  const { userId } = useLoggedIn() // This could be username, if/when you add it.
+  const { username } = useLoggedIn()
+  const usernameFormatted = username ? username[0].toUpperCase() + username.slice(1) : ''
   const searchParams = useSearchParams()
+  const isRegistration = searchParams.get('register') === 'true'
   const isLogin = searchParams.get('login') === 'true'
+  const welcomeMessage = usernameFormatted && isRegistration ? `Welcome, ${usernameFormatted}.` : usernameFormatted && isLogin ? `Welcome back, ${usernameFormatted}.` : 'Welcome back.'
 
-  return isLogin && (
-    <div>{userId ? `Welcome back, ${userId}.` : 'Welcome back.'}</div>
+  return (isLogin || isRegistration) && (
+    <div>{welcomeMessage}</div>
   )
 }
 
