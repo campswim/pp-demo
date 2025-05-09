388fc3274eac92684c2c296401b30d33fb77a910
Nathan Cox <nrc4@hotmail.com>
2025-05-09 15:28:42 -0700

    Update the call's status in the DB with 'authenticated' or 'failed.'

--- src/app/api/voice/validate-pin/route.ts
+++ src/app/api/voice/validate-pin/route.ts
@@ -1,16 +1,43 @@
 import { twiml } from 'twilio'
 import { NextRequest } from 'next/server'
+import db from '@/utils/db'
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData()
   const pin = formData.get('Digits')?.toString()
+  const callSid = formData.get('CallSid')?.toString()
   const response = new twiml.VoiceResponse()
 
+  // End the call if it is missing an SID.
+  if (!callSid) {
+    response.say('Call ID is missing. Cannot verify authentication.')
+    response.hangup()
+
+    await db.voiceCall.update({
+      where: { callSid },
+      data: { status: 'failed' }
+    })
+    
+    return new Response(response.toString(), {
+      headers: { 'Content-Type': 'text/xml' }
+    })
+  }
+
   if (pin === '1234') {
-    // TODO: mark user as authenticated (e.g. DB, Redis, or WS event)
     response.say('Authentication successful. Goodbye.')
+
+    await db.voiceCall.update({
+      where: { callSid },
+      data: { status: 'authenticated' }
+    })
   } else {
-    response.say('Incorrect PIN. Goodbye.')
+    // To-do: Add a retry mechanism.
+    response.say("I'm sorry, you've entered your PIN incorrectly. Goodbye.")
+
+    await db.voiceCall.update({
+      where: { callSid },
+      data: { status: 'failed' },
+    })
   }
 
   response.hangup()
