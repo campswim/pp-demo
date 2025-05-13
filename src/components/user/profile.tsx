afad0fc6ee6e2488841e87a935ed6e4ffbfbc254
Nathan Cox <nrc4@hotmail.com>
2025-05-13 15:51:47 -0700

    Refactor the user profile page to use the Prisma schema in the rendering of a user's info.

--- src/components/user/profile.tsx
+++ src/components/user/profile.tsx
@@ -1,17 +1,27 @@
-'use client'
-
 import { type User } from '@/generated/prisma'
+import { getPrismaSchemaFields } from '@/utils/meta'
+import { formatHeader, formatPhoneDashed } from '@/utils/helpers'
+import { decrypt } from '@/utils/encrypt-decrypt'
 
 const Profile = ({ user }: {user: User | null}) => {
+  const headers = getPrismaSchemaFields('User')
+  const excludedHeaders = ['id', 'password', 'createdAt', 'updatedAt']
+
   return (
-    <div>
-      <p>ID: {user?.id || 'N/A'}</p>
-      <p>Email: {user?.email || 'N/A'}</p>
-      <p>Role: {user?.role || 'N/A'}</p>
-      <p>Logged In: {user?.loggedIn ? 'Yes' : 'No'}</p>
-      <p>Created: {user?.createdAt?.toLocaleString() || 'N/A'}</p>
-      <p>Updated: {user?.updatedAt?.toLocaleString() || 'N/A'}</p>
-    </div>
+    <ul className='flex flex-col gap-2'>
+      {headers.map((header, idx) => (
+        header === 'phone' ? 
+        (
+          <li key={idx} className='flex gap-2'>
+            {formatHeader(header)}: {user ? formatPhoneDashed(decrypt(String(user[header as keyof User]))) : 'N/A'}
+          </li>
+        )
+        :
+        (
+          !excludedHeaders.includes(header) && <li key={idx} className='flex gap-2'>{formatHeader(header)}: {String(user?.[header as keyof User] ?? 'N/A')}</li>
+        )
+      ))}
+    </ul>
   )
 }
 
