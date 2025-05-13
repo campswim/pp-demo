3a4c8cfca7a721e03d17707bfea50b3e725b826e
Nathan Cox <nrc4@hotmail.com>
2025-05-13 15:48:05 -0700

    Refactor validate safe word to properly manage incorrect entries and provide a retry.

--- src/app/api/voice/validate-safeword/route.ts
+++ src/app/api/voice/validate-safeword/route.ts
@@ -1,27 +1,35 @@
 import { twiml } from 'twilio'
 
 export async function POST(req: Request) {
+  const url = new URL(req.url)
+  const retryCount = Number(url.searchParams.get('retry') || '0')
   const formData = await req.formData()
   const speechResult = formData.get('SpeechResult')?.toString().trim().toLowerCase()
-  const digits = formData.get('Digits')?.toString().trim().toLowerCase()
-  const transcript = (speechResult || digits)?.replace(/[^\w\s]/g, '')
+  // const digits = formData.get('Digits')?.toString().trim().toLowerCase()
+  // const transcript = (speechResult || digits)?.replace(/[^\w\s]/g, '')
+  const transcript = speechResult?.replace(/[^\w\s]/g, '')
   const response = new twiml.VoiceResponse()
   const acceptedWords = ['banana', 'pineapple']
 
-if (acceptedWords.some(word => transcript?.includes(word))) {
-    response.say('Safe word accepted. Now enter your four-digit PIN.')
+  if (acceptedWords.some(word => transcript?.includes(word))) {
+    response.say('Your safe word has been accepted.')
+    response.say('Now, using your phone\'s keypad, please enter your four-digit PIN.')
     response.gather({
+      input: ['dtmf'],
       numDigits: 4,
       action: `${process.env.BASE_URL}/api/voice/validate-pin`,
       method: 'POST'
     })
-  } else {
-    response.say('Incorrect safe word. Goodbye.')
-    response.hangup()
+  } else { // If the safe word is incorrect, ask for it again.
+    if (retryCount < 1) {
+      response.say('Your safe word is incorrect.')
+      response.redirect(`${process.env.BASE_URL}/api/voice/voice-entry?retry=${retryCount + 1}`)
+      return new Response(response.toString(), { headers: { 'Content-Type': 'text/xml' }})
+    } else {
+      response.say('Your safe word is still incorrect. Please try logging in again. Goodbye!')
+      response.hangup()
+    }
   }
 
-  const xml = response.toString()
-  return new Response(xml, {
-    headers: { 'Content-Type': 'text/xml' }
-  })
+  return new Response(response.toString(), { headers: { 'Content-Type': 'text/xml' }})
 }
