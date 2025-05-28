25122aab453b1b78895c605449ddf296e089b5bc
Nathan Cox <nrc4@hotmail.com>
2025-05-28 14:29:21 -0700

    Update the welcome message to show the background on the homepage when a user is logged in but there is no logged-in query param.

--- src/components/user/welcomeMessage.tsx
+++ src/components/user/welcomeMessage.tsx
@@ -12,17 +12,20 @@ const WelcomeMessage = () => {
   const isLogin = searchParams.get('login') === 'true'
   const welcomeMessage = usernameFormatted && isRegistration ? 'Welcome' : usernameFormatted && isLogin ? 'Welcome Back' : ''
 
-  return (isLogin || isRegistration) && (
+  return (
     <div className='relative w-full h-screen'>
       <div className='absolute inset-0 flex items-center justify-center opacity-10 pointer-events-none z-0'>
         <Logo logoSize={800}/>
       </div>
-      <div className='mt-[10%]'>
-        <h1 className='relative text-9xl z-10'>{welcomeMessage}</h1>
-        {usernameFormatted && (
-          <p className='text-7xl text-center'>{`\u2013 ${usernameFormatted} \u2013`}</p>
-        )}
-      </div>
+      {isLogin || isRegistration &&
+      (
+        <div className='mt-[10%]'>
+          <h1 className='relative text-9xl z-10'>{welcomeMessage}</h1>
+          {usernameFormatted && (
+            <p className='text-7xl text-center'>{`\u2013 ${usernameFormatted} \u2013`}</p>
+          )}
+        </div>
+      )}
     </div>
   )
 }
