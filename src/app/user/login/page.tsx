0fd05ae1ce6de3d87fe2803ff7dcc288ec3f6c52
Nathan Cox <nrc4@hotmail.com>
2025-05-29 11:41:42 -0700

    Remove h-screen.

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -3,7 +3,7 @@ import LogoBackground from '@/components/ui/logo-background'
 
 export default function LoginPage() {
   return (
-    <div className='relative w-full h-screen flex justify-center items-center'>
+    <div className='relative w-full flex justify-center items-center'>
       <LogoBackground />
       <LoginForm caller='login' />
     </div>
