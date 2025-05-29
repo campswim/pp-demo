6404d2be4810d0c11fb2b3d5be21112582deb7f4
Nathan Cox <nrc4@hotmail.com>
2025-05-29 10:26:36 -0700

    Add demo-nav-items.ts with temporary hardcoded values.

---
+++ src/data/demo-nav-items.ts
@@ -0,0 +1,8 @@
+import { NavItem } from '@/lib/types'
+
+export const demoNavItems: NavItem[] = [
+  { id: 1, type: 'demo', name: 'Checking & Savings', href: '#' },
+  { id: 2, type: 'demo', name: 'Credit Cards', href: '#' },
+  { id: 3, type: 'demo', name: 'Commercial Banking', href: '#' },
+  { id: 4, type: 'demo', name: 'Log Out', href: '/demo/logout' }
+]
