1390f53df21d685998a074d6181e7490358adaa6
Nathan Cox <nrc4@hotmail.com>
2025-05-29 13:03:05 -0700

    Remove logoSize and change absolute to fixed.

--- src/components/ui/logo-background.tsx
+++ src/components/ui/logo-background.tsx
@@ -2,8 +2,8 @@ import Logo from '@/components/navbar/logo'
 
 export default function LogoBackground() {
   return (
-    <div className='absolute inset-0 flex items-center justify-center opacity-10 pointer-events-none z-0'>
-      <Logo logoSize={800} />
+    <div className='fixed inset-0 flex items-center justify-center opacity-10 pointer-events-none z-0 overflow-hidden'>
+      <Logo />
     </div>
   )
 }
