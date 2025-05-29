5e204fc82b86919f013431d8dee6625c6c4a1614
Nathan Cox <nrc4@hotmail.com>
2025-05-29 11:43:47 -0700

    Fix the demo background to work on mobile as well as desktop.

--- src/components/ui/demo-background.tsx
+++ src/components/ui/demo-background.tsx
@@ -1,7 +1,7 @@
 export default function DemoBackground() {
   return (
     <div className='absolute inset-0 flex items-center justify-center opacity-7 pointer-events-none z-0'>
-      <p className='text-[50rem]'>DEMO</p>
+      <p className='text-[20vw] md:text-[30vw] lg:text-[40vw] xl:text-[50rem]'>DEMO</p>    
     </div>
   )
 }
