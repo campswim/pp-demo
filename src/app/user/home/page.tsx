222d00bbe58ab69402ea8aaecc3cbaa99073ac37
Nathan Cox <nrc4@hotmail.com>
2025-05-29 11:41:26 -0700

    Remove h-screen.

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -3,7 +3,7 @@ import LogoBackground from '@/components/ui/logo-background'
 
 export default function Home() {
   return (
-    <div className='relative w-full h-screen flex justify-center items-center'>
+    <div className='relative w-full flex justify-center items-center'>
       <LogoBackground />      
       <WelcomeMessage />
     </div>
