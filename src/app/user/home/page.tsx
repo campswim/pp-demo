b0d94717c435e752f8fafe8d1da0a838d6962f2e
Nathan Cox <nrc4@hotmail.com>
2025-05-30 12:03:47 -0700

    Format the instructions on the user's homepage.

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,32 +1,34 @@
 import WelcomeMessage from '@/components/user/welcomeMessage'
-import LogoBackground from '@/components/ui/logo-background'
+import Logo from '@/components/navbar/logo'
 
 export default function Home() {
   return (
     <>
       <div className='relative w-full'>
-        <LogoBackground />
-        <WelcomeMessage />
-      <div className='flex flex-col items-center justify-center'>
-        <div className='flex flex-col items-center justify-center w-full mb-5 lg:mb-15 p-4 border-b bg-blue-100 dark:bg-gray-900 text-center text-2xl lg:text-6xl lg:min-h-48'>
-          <h1>
-            How to Use the Phone-and-Pin Demo
-          </h1>
+        <div className='flex flex-col items-center justify-center'>
+          <div className='flex flex-col items-center justify-center w-full mb-5 lg:mb-15 p-4 border-b bg-blue-100 dark:bg-gray-900 text-center text-2xl lg:text-6xl lg:min-h-48'>
+            <div className='flex flex-col text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-7xl xs:flex-row items-center'>
+              <h1>How to Use the</h1>
+              <Logo hrefBoolean={false} caller='inline' />
+              <h1>Demo</h1>
+            </div>
+          </div>
+          <WelcomeMessage />
+          <ol className='list-decimal pl-10 pr-4 space-y-4 xs:pl-15 sm:text-xl sm:max-w-[80%] sm:mx-auto lg:text-3xl lg:max-w-[75%] xl:max-w-[50%]'>
+            <li>Select &quot;Demo&quot; from the footer.</li>
+            <li>Have your safe word and PIN at the ready.</li>
+            <li>Log into your demo account using your credentials for this site.</li>
+            <li>Accept the incoming phone call triggered by clicking the log-in button.</li>
+            <li>Follow the prompts of the call (referring, if needed, to your profile page for your safe word and PIN):
+              <ul className='list-disc px-4'>
+                <li>Speak your safe word.</li>
+                <li>Enter your four-digit PIN.</li>
+              </ul>
+            </li>
+            <li>Wait to be redirected to your account&#39;s page upon successful authentication.</li>
+            <li>In the event of an error, wait to be redirected to the demo log-in page and try again.</li>
+          </ol>
         </div>
-        <ol className='list-decimal pl-10 pr-4 lg:text-2xl lg:max-w-[40%]'>
-          <li>Select &quot;Demo&quot; from the footer.</li>
-          <li>Have your safe word and PIN at the ready.</li>
-          <li>Log into your demo account using your credentials for this site.</li>
-          <li>Accept the incoming phone call triggered by clicking the log-in button.</li>
-          <li>Follow the prompts of the call:</li>
-          <ul className='list-disc px-4'>
-            <li>Speak your safe word.</li>
-            <li>Enter your four-digit PIN.</li>
-          </ul>
-          <li>Wait to be redirected to your account&#39;s page upon successful authentication.</li>
-          <li>In the event of an error, wait to be redirected to the demo log-in page and try again.</li>
-        </ol>
-      </div>
       </div>
     </>
   )
