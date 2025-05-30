3b7b1b5ad11020b11614b7bd211b421b4c7674ab
Nathan Cox <nrc4@hotmail.com>
2025-05-30 12:07:09 -0700

    Add a caller parameter to the logo component and conditional rendering for inline logo.

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -6,29 +6,40 @@ import PhoneWithText from '@/components/ui/phone-with-text'
 import { TbAmpersand } from "react-icons/tb"
 import { useLoggedIn } from '@/context/loggedIn'
 
-const Logo: React.FC<{ hrefBoolean?: boolean  }> = ({ hrefBoolean = false }) => {
+const Logo: React.FC<{ hrefBoolean?: boolean, caller?: string }> = ({ hrefBoolean = false, caller = null }) => {
   const { role } = useLoggedIn()
   const href = role !== 'guest' ? '/user/home' : '/'
 
-  let h1Class = 'flex items-center text-3xl font-bold'
-  h1Class += !hrefBoolean ? ' justify-center' : ' justify-start'
+  let className = 'flex items-center text-3xl font-bold'
+  className += !hrefBoolean ? ' justify-center' : ' justify-start'
 
   return (
-    <h1 className={h1Class}>
-      {hrefBoolean ? (
+    <div className={className}>
+      {hrefBoolean ? 
+      (
         <Link href={href} className='flex items-center hover:text-blue-500'>
           <PhoneWithText className='w-20 h-20' />
           <TbAmpersand className='w-8 h-8' />
           <MdFiberPin className='w-22 h-25' />
         </Link>
-      ) : (
-        <div className='absolute inset-0 z-0 pointer-events-none flex items-center justify-center overflow-hidden'>
-          <PhoneWithText className='w-full h-auto' />
+      ) 
+      : caller === 'inline' ?
+      (
+        <div className='pointer-events-none flex items-center justify-center mx-6'>
+          <PhoneWithText className='w-10 h-auto sm:w-20 xl:w-40' />
+          <TbAmpersand className='w-[15%] h-auto' />
+          <MdFiberPin className='w-10 h-auto sm:w-20 xl:w-40' />      
+        </div>
+      )
+      :
+      (
+        <div className={`inset-0 z-0 pointer-events-none flex items-center justify-center overflow-hidden ${caller !== 'public homepage' ? 'fixed z-[-1] opacity-10' : ''}`}>
+          <PhoneWithText className='w-[80vw] h-auto max-w-none' />
           <TbAmpersand className='w-[33%] h-auto' />
-          <MdFiberPin className='w-full h-auto' />      
+          <MdFiberPin className='w-[80vw] h-auto max-w-none' />      
         </div>
       )}
-    </h1>
+    </div>
   )
 }
 
