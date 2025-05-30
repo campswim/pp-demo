897ad6a15dc033ffe468ab7ea6ec264b66ee8f25
Nathan Cox <nrc4@hotmail.com>
2025-05-30 12:05:58 -0700

    Add justify-center to the global container.

--- src/components/global/container.tsx
+++ src/components/global/container.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils'
 
 const Container = ({ children, className }: { children: React.ReactNode, className?: string }) => {
   return (
-    <div className={cn('flex flex-1 flex-col mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
+    <div className={cn('flex flex-1 flex-col justify-center mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
       {children}
     </div>
   )
