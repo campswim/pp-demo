39af5e6696100b8e8cb4c34d0f760704c357aadf
Nathan Cox <nrc4@hotmail.com>
2025-05-30 12:12:13 -0700

    Render the demo background based on pathname.

--- src/components/ui/logo-background.tsx
+++ src/components/ui/logo-background.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import Logo from '@/components/navbar/logo'
+import { usePathname } from 'next/navigation'
 
 export default function LogoBackground() {
+  const pathname = usePathname()
+
+  if (pathname.startsWith('/demo')) return null
+
   return (
-    <div className='fixed inset-0 flex items-center justify-center opacity-10 pointer-events-none z-0 overflow-hidden'>
+    <div className='fixed inset-0 flex items-center justify-center opacity-7 pointer-events-none z-0 overflow-hidden'>
       <Logo />
     </div>
   )
