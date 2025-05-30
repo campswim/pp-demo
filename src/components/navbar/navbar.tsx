2ddd0e929c409c804e68428e98cf7e24237b09fa
Nathan Cox <nrc4@hotmail.com>
2025-05-30 12:08:50 -0700

    Style the navbar for mobile.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,8 +4,8 @@ import UtilLinks from '@/components/navbar/util-links'
 
 const Navbar = ({ isDrawer }: { isDrawer: boolean }) => {
   return (
-    <nav className='relative flex flex-col items-start md:grid md:grid-cols-3 md:items-center min-h-20'>
-      <div className='w-full flex justify-center'>
+    <nav className='relative flex flex-col items-start sm:flex-row sm:items-center sm:justify-between lg:grid lg:grid-cols-3 lg:items-center min-h-15 py-4 xs:py-0'>
+      <div className='w-full flex justify-center sm:justify-start sm:max-w-[40%]'>
         <Logo hrefBoolean={true} />
       </div>
       <NavLinks isDrawer={isDrawer} />
