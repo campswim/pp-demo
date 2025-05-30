16c7e0a3c4df86a2add7fb685924ae73b5c8c5d6
Nathan Cox <nrc4@hotmail.com>
2025-05-30 12:09:34 -0700

    Position the theme toggler in mobile.

--- src/components/navbar/util-links.tsx
+++ src/components/navbar/util-links.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from '@/components/ui/mode-toggle'
 
 export default function UtilLinks() {
   return (
-    <div className='absolute right-0 bottom-0 md:static md:flex md:items-center md:justify-end md:gap-4'>
+    <div className='absolute right-0 bottom-3 xs:-bottom-1 sm:static sm:flex sm:items-center sm:justify-end sm:gap-4'>
       <ModeToggle />
     </div>
   )
