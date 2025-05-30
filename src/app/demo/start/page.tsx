abe632f77993f5990a717624972ce49fbc92a834
Nathan Cox <nrc4@hotmail.com>
2025-05-30 12:00:43 -0700

    Add conditional rendering of instructions on the demo start page, based on call status.

--- src/app/demo/start/page.tsx
+++ src/app/demo/start/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import GridLoaderClient from '@/components/ui/grid-loader'
 import { useLoggedIn } from '@/context/loggedIn'
 import { useInitiateCall } from '@/hooks/use-initiate-call'
@@ -13,6 +13,8 @@ import Demo from '@/components/ui/demo-background'
 export default function DemoStart() {
   const [callStatus, setCallStatus] = useState<CallStatus>('')
   const [errorMessage, setErrorMessage] = useState<string | null>(null)
+  const showMessageOne = useRef(true)
+  const showMessageTwo = useRef(true)
   const { userId } = useLoggedIn()
 
   // Trigger the call when the component mounts and the userId is available.
@@ -24,12 +26,17 @@ export default function DemoStart() {
   // Stop polling if the call is completed or an error occurs.
   useCallRouting(callStatus)
 
+  useEffect(() => {
+    if (callStatus === 'success') showMessageOne.current = false
+    if (callStatus === 'ringing') showMessageTwo.current = false
+  }, [callStatus])
+
   return (
     <div className='relative w-full flex items-center'>
       <Demo />
       <div className='text-center'>
-        <p>Stand by for an incoming phone call to your phone on record.</p>
-        <p>Please have your safe word and PIN at the ready.</p>
+        {showMessageOne.current && <p className='text-3xl lg:text-5xl p-4 mb-6'>Stand by for an incoming phone call to your phone on record.</p>}
+        {showMessageTwo.current && <p className='text-2xl lg:text-4xl p-4'>Please have your safe word and PIN at the ready.</p>}
         <GridLoaderClient />
         {callStatus !== 'error' && <p className='text-green-500'>{statusMap[callStatus]}</p>}
         {callStatus === 'error' && <p className='text-red-500'>Error: {errorMessage}</p>}
