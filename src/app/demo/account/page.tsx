68142820968d399f7e212804319b3d0cb5816597
Nathan Cox <nrc4@hotmail.com>
2025-05-30 15:55:10 -0700

    Style the demo account's page in mobile.

--- src/app/demo/account/page.tsx
+++ src/app/demo/account/page.tsx
@@ -2,9 +2,9 @@ import DemoBackground from '@/components/ui/demo-background'
 
 export default function DemoAccount() {
   return (
-    <div>
-      <div className='p-6'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+    <div className='w-full max-w-[90%] mb-6 min-h-screen'>      
+      <div className='py-6 w-full max-w-none'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6'>          
           {[
             { type: 'Credit Card', amount: '$1,254.67' },
             { type: 'Savings', amount: '$8,942.20' },
@@ -15,14 +15,14 @@ export default function DemoAccount() {
           ].map((account, index) => (
             <div
               key={index}
-              className='bg-white dark:bg-gray-900 border border-gray-300 shadow-lg rounded-lg py-4 px-4 h-40 min-w-[20rem] flex flex-col justify-center'
+              className='bg-white dark:bg-gray-900 border border-gray-300 shadow-lg rounded-lg p-4 flex flex-col'
             >
-              <div className='flex flex-col items-center justify-between h-full'>
-                <div className='w-full flex justify-between mt-4'>
-                  <h2 className='text-xl font-semibold break-words whitespace-normal w-0 flex-1'>{account.type}</h2>
+              <div className='flex flex-col justify-between'>
+                <div className='flex justify-between mt-4'>
+                  <h2 className='text-xl font-semibold md:break-words md:whitespace-normal w-0 flex-1'>{account.type}</h2>
                   <p className='text-2xl font-bold text-gray-700 dark:text-gray-200'>{account.amount}</p>
                 </div>
-                <button className='w-full  bg-blue-100 dark:bg-white/70 text-white p-2 rounded hover:bg-sky-400 dark:hover:bg-sky-700 transition-colors'>
+                <button className='mt-6 bg-blue-100 dark:bg-white/70 text-black dark:text-white p-2 rounded hover:bg-sky-400 dark:hover:bg-sky-700 transition-colors'>
                   View Account
                 </button>
               </div>
