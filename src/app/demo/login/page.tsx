834229af4b8174e3cb48c0ad36b78b0a104f91d5
Nathan Cox <nrc4@hotmail.com>
2025-07-10 14:53:46 -0700

    Center the log-in form on the page.

--- src/app/demo/login/page.tsx
+++ src/app/demo/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/components/user/login-form'
 
 export default function DemoLogin() {
   return (
-    <div className='w-full flex justify-center'>
+    <div className='fixed inset-0 w-full flex justify-center items-center'>
       <LoginForm caller='demo' />
     </div>
   )
