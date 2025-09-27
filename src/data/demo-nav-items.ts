a08741129fb5a30b0511cc18c350c793f04978b1
Nathan Cox <nrc4@hotmail.com>
2025-09-27 16:02:46 -0700

    Update the rendering of the demo's navlinks to reflect the current page.

--- src/data/demo-nav-items.ts
+++ src/data/demo-nav-items.ts
@@ -4,5 +4,7 @@ export const demoNavItems: NavItem[] = [
   { id: 1, type: 'demo', name: 'Checking & Savings', href: '#' },
   { id: 2, type: 'demo', name: 'Credit Cards', href: '#' },
   { id: 3, type: 'demo', name: 'Commercial Banking', href: '#' },
-  { id: 4, type: 'demo', name: 'Log Out', href: '/demo/logout' }
+  { id: 4, type: 'demo', name: 'Log In', href: '/demo/login'},
+  { id: 5, type: 'demo', name: 'Log Out', href: '/demo/logout' },
+  { id: 6, type: 'demo', name: 'Register', href: '#' },
 ]
