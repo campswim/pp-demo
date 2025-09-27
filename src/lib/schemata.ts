d019f8e76be1268121ad96a95aa51623463ec006
Nathan Cox <nrc4@hotmail.com>
2025-09-27 16:23:27 -0700

    Correct error messaging on registration page.

--- src/lib/schemata.ts
+++ src/lib/schemata.ts
@@ -8,7 +8,7 @@ export const LoginFormSchema = z.object({
   // email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
   password: z
     .string()
-    .min(5, { message: 'be at least 5 characters long.' })
+    .min(5, { message: 'must be at least 5 characters long.' })
     // .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
     // .regex(/[0-9]/, { message: 'Contain at least one number.' })
     // .regex(/[^a-zA-Z0-9]/, {
