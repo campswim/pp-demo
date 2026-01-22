dea895459001f7b7ee222cbce8ddc2b5b8b6a75d
Nathan Cox <nrc4@hotmail.com>
2026-01-21 19:13:58 -0800

    Update navlinks for the demo; style the demo's navbar; adjust the demo's navlink type.

--- src/components/demo/demo-header.tsx
+++ src/components/demo/demo-header.tsx
@@ -12,42 +12,41 @@ export default function DemoHeader() {
   const pathname = usePathname()
   const [open, setOpen] = useState(false)
   const shouldShowNavItem = (href: string) => {
+    // On landing page: hide everything but Log In.
     if (pathname === '/demo/landing-page') {
-      // On landing page: hide everything but Log In.
       if (href !== '/demo/login') return false
       return true
     }
 
+    // On log-in page: hide Register & Log Out.
     if (pathname === '/demo/login') {
-      // On login page: hide Register & Log Out.
       if (href === '/demo/register-creds' || href === '/demo/logout') return false
       return true
     }
 
+    // On register page: hide Login.
     if (pathname === '/demo/register-creds') {
-      // On register page: hide Login
       if (href === '/demo/login') return false
       return true
     }
 
+    // On account page: hide log-in & register.
     if (pathname === '/demo/account') {
-      // On account page: hide Login & Register
       if (href === '/demo/login' || href === '/demo/register-creds') return false
       return true
     }
 
-    // Default: show everything
+    // Default: show everything.
     return true
   }
   
   if (!pathname.startsWith('/demo')) return null
 
   return (
-    <header className='fixed w-full min-h-15 lg:min-h-25 flex items-center border-b bg-gradient-to-r from-[#1e3a6a] to-[#173054] backdrop-blur z-50'>
-      <div className='w-full lg:max-w-7xl lg:mx-auto p-4 flex items-center'>
-        {/* <nav className='hidden md:flex justify-center gap-6 col-start-2'> */}
+    <header className='fixed inset-x-0 w-full min-h-15 lg:min-h-25 flex items-center border-b bg-gradient-to-r from-[#1e3a6a] to-[#173054] backdrop-blur z-50'>
+      <div className='w-full p-4 flex items-center'>
         <nav className='relative w-full flex flex-col items-start sm:flex-row sm:items-center sm:justify-between lg:grid lg:grid-cols-3 lg:items-center py-4 xs:py-0'>
-          <Link href='/demo/landing-page' className='text-xl font-bold lg:justify-self-center lg:col-start-1'>
+          <Link href='/demo/landing-page' className='text-xl font-bold lg:justify-self-start lg:col-start-1'>
             <Image 
               src='/logos/Fortress_Demo_3-removebg-preview.png'
               alt='Fortress Demo Logo'
@@ -57,17 +56,39 @@ export default function DemoHeader() {
             />
           </Link>
           <div className="flex justify-center gap-6 lg:col-start-2">
-            {demoNavItems.map(({ id, name, href }) => {
-              if (!shouldShowNavItem(href)) return null
-                const isActive = pathname === href ? 'border-b-2 border-blue-500' : ''
-                
+            {demoNavItems
+              .filter(({ name, auth }) => {
+                if (pathname === '/demo/account') {
+                  return auth && name !== 'Log In' && name !== 'Log Out' && name !== 'Profile'
+                }
+                return true 
+              })
+              .map(({ id, name, href }) => {
+                if (!shouldShowNavItem(href)) return null
+                const isActive = pathname === href ? 'border-b-2 border-white' : ''
                 return (
-                <Link key={id} href={href} className={`text-xl text-white hover:text-primary ${isActive}`}>
-                  {name}
-                </Link>
-              )})}
-            </div>
-          <div className="lg:col-start-3 lg:justify-self-center"></div>
+                  <Link key={id} href={href} className={`text-xl text-white hover:text-primary ${isActive}`}>
+                    {name}
+                  </Link>
+                )
+              })}
+          </div>
+
+          <div className="flex justify-end gap-6 lg:col-start-3 lg:justify-self-end">
+            {demoNavItems
+              .filter(({ name }) => {
+                return name === 'Log In' || name === 'Log Out' || name === 'Profile'
+              })
+              .map(({ id, name, href }) => {
+                if (!shouldShowNavItem(href)) return null
+                const isActive = pathname === href ? 'border-b-2 border-white' : ''
+                return (
+                  <Link key={id} href={href} className={`text-xl text-white hover:text-primary ${isActive}`}>
+                    {name}
+                  </Link>
+                )
+              })}
+          </div>
         </nav>
         <div className='md:hidden'>
           <Sheet open={open} onOpenChange={setOpen}>
@@ -78,7 +99,9 @@ export default function DemoHeader() {
               <SheetTitle className='sr-only'>Demo Menu</SheetTitle>
               <SheetDescription className='sr-only'>This is the demo navigation menu.</SheetDescription>
               <div className='mt-8 flex flex-col space-y-4'>
-                {demoNavItems.map(({ id, name, href }) => {
+                {demoNavItems.map(({ id, name, href, auth }) => {
+                  if (pathname === '/demo/account' && !auth) return null
+
                   if (!shouldShowNavItem(href)) return null
 
                   return (
--- src/data/demo-nav-items.ts
+++ src/data/demo-nav-items.ts
@@ -1,10 +1,14 @@
 import { NavItem } from '@/lib/types'
 
 export const demoNavItems: NavItem[] = [
-  { id: 1, type: 'demo', name: 'Accounts', href: '#' },
-  // { id: 2, type: 'demo', name: 'Credit Cards', href: '#' },
-  // { id: 3, type: 'demo', name: 'Commercial Banking', href: '#' },
-  { id: 4, type: 'demo', name: 'Log In', href: '/demo/login'},
-  { id: 5, type: 'demo', name: 'Log Out', href: '/demo/logout' },
-  { id: 6, type: 'demo', name: 'Register', href: '/demo/register-creds' },
+  { id: 1, type: 'demo', auth: true, name: 'Accounts', href: '/demo/account' },
+  { id: 2, type: 'demo', auth: false, name: 'Credit Cards', href: '#' },
+  { id: 3, type: 'demo', auth: false, name: 'Commercial Banking', href: '#' },
+  { id: 4, type: 'demo', auth: false, name: 'Loans', href: '#' },
+  { id: 5, type: 'demo', auth: true, name: 'Transfer', href: '#' },
+  { id: 6, type: 'demo', auth: true, name: 'Rewards', href: '#' },
+  { id: 7, type: 'demo', auth: true, name: 'Profile', href: '#'},
+  { id: 8, type: 'demo', auth: false, name: 'Log In', href: '/demo/login'},
+  { id: 9, type: 'demo', auth: true, name: 'Log Out', href: '/demo/logout' },
+  { id: 10, type: 'demo', auth: false, name: 'Register', href: '/demo/register-creds' },
 ]
