400520454ae34bafa45a68c022b26c38428c5d71
Nathan Cox <nrc4@hotmail.com>
2026-01-25 18:37:48 -0800

    Add padding to the bottom of the main header.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,7 +4,7 @@ import UtilLinks from '@/components/navbar/util-links'
 
 const Navbar = ({ isDrawer }: { isDrawer: boolean }) => {
   return (
-    <nav className='relative flex flex-col items-start sm:flex-row sm:items-center sm:justify-between lg:grid lg:grid-cols-[1fr_2fr_1fr] lg:items-center'>
+    <nav className='relative flex flex-col items-start py-4 sm:flex-row sm:items-center sm:justify-between lg:grid lg:grid-cols-[1fr_2fr_1fr] lg:items-center'>
       <div className='w-full max-h-30 flex justify-center sm:justify-start sm:max-w-[40%] lg:justify-self-start'>
           <Logo hrefBoolean={true} />
       </div>
